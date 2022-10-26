00829b2eda2e2b58432551f35441b3e42a8ffc41
Ansley Lewis <ansleylewis@Ansley-Lewis-MacBook-Pro.local>
2022-10-26 13:54:21 -0400

    added the functions

--- discussion_8.py
+++ discussion_8.py
@@ -6,29 +6,48 @@ import unittest
 # search for the url in the University of Michgian wikipedia page (in the third pargraph of the intro)
 # HINT: You will have to add https://en.wikipedia.org to the URL retrieved using BeautifulSoup
 def getLink(soup):
+    a_tag = soup.find('a', title= "List of American universities with Olympic medals")
+    print ("en.wikipedia.org" + a_tag.get('href'))
+    return "en.wikipedia.org" + a_tag.get('href')
+
     
     pass
 
 # Task 3: Get the details from the box titled "College/school founding". Get all the college/school names and the year they were
 # founded and organize the same into key-value pairs.
 def getAdmissionsInfo2019(soup):
-
-    pass
+    table_tag = soup.find('table', class_= 'toccolours')
+    tr_list = table_tag.final_all('tr')
+    d = {}
+    for tr_tag in tr_list[1:]:
+        td_list = tr_tag.find_all('td')
+        school_name = td_list[0].text.strip()
+        found_year = td_list[1].text.strip()
+        d[school_name] = found_year
+        
+    return d
 
 
 
 def main():
     # Task 1: Create a BeautifulSoup object and name it soup. Refer to discussion slides or lecture slides to complete this
-
+    
     #### YOUR CODE HERE####
 
     #Call the functions getLink(soup) and getAdmissionsInfo2019(soup) on your soup object.
+
+    url = 'https://en.wikipedia.org/wiki/University_of_Michigan'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
     getLink(soup)
     getAdmissionsInfo2019(soup)
 
+
 class TestAllMethods(unittest.TestCase):
+
     def setUp(self):
         self.soup = BeautifulSoup(requests.get('https://en.wikipedia.org/wiki/University_of_Michigan').text, 'html.parser')
+
 
     def test_link_nobel_laureates(self):
         self.assertEqual(getLink(self.soup), 'https://en.wikipedia.org/wiki/List_of_American_universities_with_Olympic_medals')
